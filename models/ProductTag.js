f73e4aa962a9d8b6466bddde2b35f74713a817bc
Kaid <kjjackson261@gmail.com>
2021-05-09 23:52:13 -0700

    upd

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require("../../../../../homework/Mapping/Develop/config/connection");
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
